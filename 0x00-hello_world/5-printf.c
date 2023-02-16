e477ca454abff44c836aec5d29e123ebf4bf8a55
john <johnkokolo@gmail.com>
2023-02-16 15:56:22 -0800

    this question is difficult

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#iclude 
+#include <Stdio.h>
+/**
+* main - program to print a string
+* Return: Always 0 (success)
+*/
+
+int man(void)
+{
+	printf("proper programming, always great,\n");
+	return (0);
+}
+ 
