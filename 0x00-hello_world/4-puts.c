41d2de180eacdb00414089a3091aa50e14efbd40
john <johnkokolo@gmail.com>
2023-02-16 15:45:13 -0800

    4th work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main (avoid)
+{
+         puts("\"Programming is like building a multilanguage");
+                 return (0);
+}		 
