81a3704977f46b2d833b335d01c01659147b22a6
john <johnkokolo@gmail.com>
2023-02-16 17:21:50 -0800

    second

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <Stdio.h>
 /**
-* main - always learning 
-* Return: Always 0 success
-*/
-
+ * main - always learning 
+ * Return: Always 0 success
+ */
 int man(void)
 {
-	printf("proper programming, always great");
+	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
