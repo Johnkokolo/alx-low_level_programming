408ed0f860323f7b04ce005074d59a570c65e849
john <johnkokolo@gmail.com>
2023-02-16 16:58:31 -0800

    difficut one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point 
- * Return: Always 0 success
+ * main - learning process
+ *
+ * Return: always return
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
-}	
+}
