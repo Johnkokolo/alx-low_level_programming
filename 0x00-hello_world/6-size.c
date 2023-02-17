735751e418355929f46231e4da39da7fba98f9de
john <johnkokolo@gmail.com>
2023-02-16 18:15:16 -0800

    6th question

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the 6th work 
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	printf("size of char: %l byte(s)\n", sizeof(char));
+	printf("size of an int: %2 byte(s)\n", sizeof(int));
+	printf("size of long int: %3 byte(s)\n", sizeof(long int));
+	printf("size of long long int: %4 byte(s), sizeof(long long int));
+	printf("size of float: %5 byte(s)\n", sizeof(float);
+	return (0);
+}	
+
