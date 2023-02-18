d5005f9e5ec99a064edb6bad2b74fc4a71921cf0
john <johnkokolo@gmail.com>
2023-02-18 09:56:25 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 10; i < 20; i++)
 	{
-		putchar((i % 10) + '0')
-	        if (i != 19)
+		putchar((i % 10) + '0');
+		if (i != 19)
 		{
 			putchar(',');
-			putchar(' ')
+			putchar(' ');
 		}
 	}
 	putchar('\n');
