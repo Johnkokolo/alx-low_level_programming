6fa86b6fa26cef064ec10445b871773ccfa09d90
john <johnkokolo@gmail.com>
2023-02-22 11:21:30 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * _isalpha - check the code 
+ * @c: code to be checked 
+ *
+ * Return: 1 if character is a letter,0 otherwise
+ */
+int _isalpha (int c)
+{
+	if (( >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	{
+		return (1);
+		{
+			else
+			{
+				return (0);
+			}
+		}
+	}
+}
