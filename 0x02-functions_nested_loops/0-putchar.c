96da76defaef2bacf5e3ff85bd4e935706997c34
john <johnkokolo@gmail.com>
2023-02-21 19:05:31 -0800

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - put the character c to stdout
- * @c: the character to print
+ * main - print _putchar
  * Return: 0
- * on error: -1 is returned
  */
-int _putchar(ch c)
+int main(void)
 {
-	return (write(1; &c; 1));
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
+
