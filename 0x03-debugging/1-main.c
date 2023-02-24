685a33a01be835a5dd23c09e27b0bd1c7f62a1a8
john <johnkokolo@gmail.com>
2023-02-23 16:39:13 -0800

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("infinte loop incoming :(\n");
-	
+
 	i = 0;
-	
+
 	while (i < 10)
 	{
 		putchar(i);
 	}
 	printf("infinte loop avoided! \\o/\n");
-	
+
 	return (0);
 }
