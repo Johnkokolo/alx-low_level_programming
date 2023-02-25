6bbb603e7feab7a1e51bb65ea5ed50af39cc2664
john <johnkokolo@gmail.com>
2023-02-24 21:41:24 -0800

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or negative(i);
 	
 	return (0);
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,10 +3,16 @@
 void positive_or_negative(int i)
 {
 	if (i > 0)
+	{
 		printf("%d is positive\n", i);
+	}
 	else if (i < 0)
+	{
 		printf("%d is negative\n", i);
-	else 
+	}
+	else
+	{	
 		printf("%d is zero\n", i);
+	}
 }
 
