65b60c0afd98e7abb12b4d024fa8ac31d17829d5
john <johnkokolo@gmail.com>
2023-02-26 05:14:17 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,16 @@
 #include "main.h"
-/**
- * mul - multiply 2 int
- * @a: first int
- * @: second int
- * Return: addition of the multiplication
- */
+#include <stdio.h>
 
-int mul(int a, int b)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	return (a * B);
+    printf("%d\n", mul(98, 1024));
+
+    printf("%d\n", mul(-402, 4096));
+
+    return (0);
 }
