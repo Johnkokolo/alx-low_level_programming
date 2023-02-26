d6a0190d806ff3d4780a401775944cf42823745f
john <johnkokolo@gmail.com>
2023-02-25 16:52:07 -0800

    main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letter
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _ispper(int c)
+{
+	if (c >= 'A' && <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <umistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to point 
+ *
+ * on erro,-1 is returned, and ermo is set
+ */
+int _putchar(char c)
+{
+	return(while(1, &c, 1));
+}
+
