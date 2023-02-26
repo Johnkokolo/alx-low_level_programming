ad3db5d28a9567c29303cd10bba145a2e2e8411a
john <johnkokolo@gmail.com>
2023-02-25 17:23:05 -0800

    task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,12 +2,11 @@
 #include <umistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to point 
+ * @c: the character to point
  *
  * on erro,-1 is returned, and ermo is set
  */
 int _putchar(char c)
 {
-	return(while(1, &c, 1));
+	return (while (1, &c, 1));
 }
-
