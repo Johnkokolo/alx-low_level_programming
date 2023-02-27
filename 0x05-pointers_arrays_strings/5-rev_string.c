4d4b046060e9de9d74feba9ab80eb1a441f3de25
john <johnkokolo@gmail.com>
2023-02-27 15:03:53 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - string reversal
+ * @s: input string
+ * Return: string reverse
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int z = 0;
+	int a;
+
+	while (s[z] != '\0')
+	{
+		z++;
+	}
+	for (a = 0; a  < z; a++)
+	{
+		z--;
+		rev = s[a];
+		s[a] = s[z];
+		s[z] = rev;
+	}
+}	
