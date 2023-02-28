97bbe41a64d89aad662b7ef5f44566ea6e069a0d
john <johnkokolo@gmail.com>
2023-02-27 18:11:12 -0800

    task 8

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,12 @@ void puts2(char *str)
         int j = 0;
 
         while (str[j] != '\0')
-        {
-                j++;
-        }
-        for (i = 0; i < j; i += 2)
-        {
-                _putchar(str[i]);
-        }
-        _putchar('\n');
+	{
+		j++;
+	}
+	for (i = 0; i < j; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
