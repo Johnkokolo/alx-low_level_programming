32eb340e8b2a97c9f0264e8ed9ea76ce51bd08eb
john <johnkokolo@gmail.com>
2023-02-28 18:52:14 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,7 +15,7 @@ char *_strcpy(char *dest, char *src)
         {       
                 l++;
         }               
-        for (x = 0; x < 1; x++;)
+        for (x = 0; x < 1; x++)
         {       
                 dest[x] = src[x];
         }
