8e2764bd07a8031b4279cb70a0db943de97cdc87
john <johnkokolo@gmail.com>
2023-03-06 10:56:46 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcp - a function that copies a mamory area
  * @dest: memory where is stored
  * @src: memory where is copied
- * *@n: number of bytes
+ * @n: number of bytes
  *
  * Return: copied memory with n byte changed
  */
@@ -12,6 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
+
 
 	for (; r < i; r++)
 	{
