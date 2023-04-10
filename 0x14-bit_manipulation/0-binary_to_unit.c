9ba0fb0c5bde4233de1472b86b8eb005b24b2a6b
Johnkokolo <johnkokolo@rocketmail.com>
2023-04-10 08:54:20 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -8,16 +8,16 @@
  */
 unsigned int binary_to_unit(const char *b)
 {
-	int i;
+	int j;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; b[i]; j++)
+	for (j = 0; b[j]; j++)
 	{
-		if (b[i] < '0' || > '1')
+		if (b[j] < '0' || b[j] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[j] - '0');
 	}
 
 	return (dec_val);
