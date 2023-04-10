628a8b1831d0b98a2fe0338a9ecfa5bbefa1b647
Johnkokolo <johnkokolo@rocketmail.com>
2023-04-10 08:36:55 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - a binary number converted to unsigned int
+ * @b: string that contains binary number
+ *
+ * Return: 0
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	int j;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+	for (j = 0; b[j]; j++)
+	{
+		if (b[j] < '0' || > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[j] - '0');
+	}
+
+	return (dec_val);
+}
