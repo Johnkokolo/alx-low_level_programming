a7297c0f2f916a5fbab1ca62d19fb828b409cb46
Johnkokolo <johnkokolo@rocketmail.com>
2023-04-11 14:41:36 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - file need to be created.
+ * @n: the name of the file that need to be created.
+ * @s: the string to write to the file created.
+ *
+ * Return: -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int jk, m, len = 0;
+
+	if (n == NULL)
+		return (-1);
+
+	if (s != NULL)
+	{
+		for (len = 0; s[len];)
+			len++;
+	}
+
+	jk = open(n, O_CREAT | O_RDWR | O_TRUNC, 0300);
+	m = write(jk, s, len);
+
+	if (jk == -1 || m == -1)
+		return (-1);
+
+	close(jk);
+
+	return (1);
+}
