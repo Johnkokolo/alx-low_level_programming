66fec5656efae7ee3a1120ff0814e6a51b3d5712
Johnkokolo <johnkokolo@rocketmail.com>
2023-04-11 15:15:24 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - always creat a file.
+ * @filename: name of the file
+ * @text_content: string to be added on the file.
+ *
+ * Return: -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int l, m, t = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (t = 0; text_content[t];)
+			t++;
+	}
+
+	l = open(filename, O_WRONLY | O_APPEND);
+	m = write(l, text_content, t);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
