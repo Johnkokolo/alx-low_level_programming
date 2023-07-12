b63db5665e4c18f126796e4a60108aa9cfed95c6
john kokolo <johnkokolo@rocketmail.com>
2023-07-12 16:16:58 -0700

    creat a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	jl = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	l = write(jl, text_content, j);
 
-	if (jk == -1 || l == -1)
+	if (jl == -1 || l == -1)
 		return (-1);
 
 	close(jl);
