203bd1ea2aa5c40457a7560456200f3c9f4b5589
john kokolo <johnkokolo@rocketmail.com>
2023-07-14 16:54:52 -0700

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,103 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int jl);
+
+/**
+ * create_buffer - 1024 bytes for a buffer allocated.
+ * @file: The file buffer is storing chars for.
+ *
+ * Return: always 0.
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buf);
+}
+
+/**
+ * close_file - file descriptors closed.
+ * @jl: The file descriptor to be closed.
+ */
+void close_file(int jl)
+{
+	int i;
+
+	i = close(jl);
+
+	if (i == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close argc %d\n", jl);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, z, i;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buf = create_buffer(argv[2]);
+	f1 = open(argv[1], O_RDONLY);
+	z = read(f1, buf, 1024);
+	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (f1 == -1 || z == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+
+		i = write(f2, buf, z);
+		if (f2 == -1 || i == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+
+		z = read(f1, buf, 1024);
+		f2 = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buf);
+	close_file(f1);
+	close_file(f2);
+
+	return (0);
+}
