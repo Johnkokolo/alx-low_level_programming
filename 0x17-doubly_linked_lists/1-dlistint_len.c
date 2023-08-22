84184007d08ae05563140ab59613677a3e964a86
john kokolo <johnkokolo@rocketmail.com>
2023-08-22 16:06:35 -0700

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,20 +10,16 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-
+	
 	count = 0;
-
 	if (h == NULL)
 		return (count);
-
 	while (h->prev != NULL)
 		h = h->prev;
-
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
